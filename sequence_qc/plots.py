142fd0fe8fd0860355c3ee8b7e5248be8ee50ccd
ionox0 <ionox0@gmail.com>
2020-07-29 16:39:12 -0400

    add more fields to noise plot tooltip

--- sequence_qc/plots.py
+++ sequence_qc/plots.py
@@ -15,5 +15,11 @@ def plot_top_noisy_positions(noisy_pileup_df: pd.DataFrame, sample_id: str = '')
     noisy_pileup_df = noisy_pileup_df.sort_values('noise_acgt', ascending=False)
     noisy_pileup_df['chrom_pos'] = noisy_pileup_df['chrom'] + ':' + noisy_pileup_df['pos'].astype(str)
     title = 'Top 100 Noisy Positions for sample {}'.format(sample_id)
-    fig = px.bar(noisy_pileup_df[:100], x='chrom_pos', y='noise_acgt', title=title)
+    fig = px.bar(
+        noisy_pileup_df[:100],
+        x='chrom_pos',
+        y='noise_acgt',
+        title=title,
+        hover_data=['ref', 'A', 'C', 'G', 'T', 'minor_allele_count', 'major_allele_count']
+    )
     fig.write_html(sample_id + TOP_NOISE_PLOT)
