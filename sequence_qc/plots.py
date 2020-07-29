036c704abf0c3e3e84d8d9b49ef28e63e832e7c4
ionox0 <ionox0@gmail.com>
2020-07-29 12:37:54 -0400

    sort noise plot

--- sequence_qc/plots.py
+++ sequence_qc/plots.py
@@ -1,8 +1,5 @@
-import plotly.express as px
-
-import numpy as np
 import pandas as pd
-# from matplotlib import pyplot as plt
+import plotly.express as px
 
 
 TOP_NOISE_PLOT = 'noisy_positions.html'
@@ -15,16 +12,7 @@ def plot_top_noisy_positions(noisy_pileup_df: pd.DataFrame, output_prefix: str =
     :param pileup_df:
     :return:
     """
-    # plt.clf()
-    # plt.figure(figsize=(10, 5))
-    # pos = np.arange(len(noisy_pileup_df))
-    # noise = noisy_pileup_df['noise_acgt']
-    # x_lab = noisy_pileup_df['chrom'] + ':' + noisy_pileup_df['pos'].astype(str)
-    # plt.bar(pos, noise, align="center", color="black")
-    # plt.xticks(ticks=pos, labels=x_lab, rotation=90, ha="center")
-    # plt.title('Top Noisy Positions')
-    # plt.savefig(TOP_NOISE_PLOT)
-
+    noisy_pileup_df = noisy_pileup_df.sort_values('noise_acgt')
     noisy_pileup_df['chrom_pos'] = noisy_pileup_df['chrom'] + ':' + noisy_pileup_df['pos'].astype(str)
     fig = px.bar(noisy_pileup_df, x='chrom_pos', y='noise_acgt')
     fig.write_html(output_prefix + TOP_NOISE_PLOT)
