af9abe4b19f39fde334425123652be737a19b799
Ian Johnson <ionox0@gmail.com>
2020-08-27 18:06:01 -0400

    merge with origin

--- sequence_qc/__init__.py
+++ sequence_qc/__init__.py
@@ -2,8 +2,4 @@
 
 __author__ = """Ronak Shah"""
 __email__ = 'rons.shah@gmail.com'
-<<<<<<< HEAD
-__version__ = '0.1.16'
-=======
 __version__ = '0.1.17'
->>>>>>> 101f08ba05e2682189150940a000ee7f867a4e86
