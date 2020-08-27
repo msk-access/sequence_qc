5b3eda435cb98e71fd5426a3c5c6de3f6826ddfd
Ian Johnson <ionox0@gmail.com>
2020-08-27 18:06:23 -0400

    Bump version: 0.1.17 → 0.1.18

--- setup.py
+++ setup.py
@@ -73,6 +73,6 @@ setup(
     test_suite='tests',
     #tests_require=req_file("requirements_dev.txt"),
     url='https://github.com/msk-access/sequence_qc',
-    version='0.1.17',
+    version='0.1.18',
     zip_safe=False,
 )
